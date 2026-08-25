8ac48df6578f5406c533016ddea9d5f1d4f6567d
igor gomes <gomesoliveirasl02@hotmail.com>
2026-08-24 21:27:32 -0300

    create routes

---
+++ src/routes/appointment.routes.ts
@@ -0,0 +1,56 @@
+import { Router } from "express";
+
+interface Appointment {
+  id: string;
+  clientName: string;
+  clientNumber: string;
+  service: string;
+  date: string;
+  time: string;
+}
+
+const appointments: Appointment[] = [];
+
+const appointmentRoutes = Router();
+
+appointmentRoutes.get("/", (request, response) => {
+  return response.json(appointments);
+});
+
+appointmentRoutes.post("/", (request, response) => {
+  const { clientName, clientNumber, service, date, time } = request.body;
+
+  if (!service || !date || !time || !clientName || !clientNumber) {
+    return response.status(400).json({
+      message: "Todos os campos são obrigatórios",
+    });
+  }
+
+  const isTimeOccupied = appointments.some(
+    (appointment) => appointment.date === date && appointment.time === time,
+  );
+
+  if (isTimeOccupied) {
+    return response.status(409).json({
+      message: "Este horário já está ocupado.",
+    });
+  }
+
+  const newAppointment: Appointment = {
+    id: crypto.randomUUID(),
+    clientName,
+    clientNumber,
+    service,
+    date,
+    time,
+  };
+
+  appointments.push(newAppointment);
+
+  return response.status(201).json({
+    message: "Agendamento criado com sucesso",
+    appointment: newAppointment,
+  });
+});
+
+export { appointmentRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,18 @@
 import express from "express";
 
+import { appointmentRoutes } from "./routes/appointment.routes.js";
+
 const app = express();
-app.use(express.json());
-
-interface Appointment {
-  id: string;
-  clientName: string;
-  clientNumber: string;
-  service: string;
-  date: string;
-  time: string;
-}
-
-const appointments: Appointment[] = [];
 
 const port = 3333;
 
-app.get("/appointments", (request, response) => {
-  return response.json(appointments);
-});
+app.use(express.json());
+
+app.use("/appointments", appointmentRoutes);
 
 app.get("/", (request, response) => {
   return response.json({
     message: "API da barbearia funcionando",
-  });
-});
-
-app.post("/appointments", (request, response) => {
-  const { clientName, clientNumber, service, date, time } = request.body;
-
-  if (!service || !date || !time || !clientName || !clientNumber) {
-    return response.status(400).json({
-      message: "Todos os campos são obrigatórios",
-    });
-  }
-
-  const isTimeOccupied = appointments.some(
-    (appointment) => appointment.date === date && appointment.time === time,
-  );
-
-  if (isTimeOccupied) {
-    return response.status(409).json({
-      message: "Este horário já está ocupado.",
-    });
-  }
-
-  const newAppointment: Appointment = {
-    id: crypto.randomUUID(),
-    clientName,
-    clientNumber,
-    service,
-    date,
-    time,
-  };
-
-  appointments.push(newAppointment);
-
-  return response.status(201).json({
-    message: "Agendamento criado com sucesso",
-    appointment: newAppointment,
   });
 });
 
